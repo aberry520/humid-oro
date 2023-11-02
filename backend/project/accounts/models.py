9f0db01484d0cccd2ee395498368111e073d1ad7
Austin Berry <aberry52098@gmail.com>
2023-11-02 15:41:25 -0400

    remove import settings

--- backend/project/accounts/models.py
+++ backend/project/accounts/models.py
@@ -1,12 +1,11 @@
 from django.db import models
-# from django.conf import settings
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar = models.ImageField(default="default.jpg", upload_to="profile/")
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    avatar = models.ImageField(default="profile/download.png", upload_to="profile/")
     def __str__(self):
         return self.user.username
     
