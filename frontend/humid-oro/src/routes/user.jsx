0a406d8e38259aac85694738659ca7ef1a9a9a5e
Austin Berry <aberry52098@gmail.com>
2023-11-02 15:25:06 -0400

    fetch user data from dj-rest-auth and check to see if logged in

--- frontend/humid-oro/src/routes/user.jsx
+++ frontend/humid-oro/src/routes/user.jsx
@@ -1,27 +1,41 @@
-import { redirect } from "react-router-dom"
+import { redirect, useLoaderData } from "react-router-dom"
+import Cookies from 'js-cookie'
+import { useAuth } from "../AuthContext";
 
 export async function loader() {
+    // const {isAuth} = useAuth();
+    // const token = localStorage.getItem("access_token");
+    console.log(Cookies.get())
     try{
-        const url = `http://localhost:8001/`
-        const cigar = await fetch(url, {
+        const url = `http://127.0.0.1:8001/dj-rest-auth/user/`
+        const userdata = await fetch(url, {
             headers: {
-              Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+                'Content-Type': 'application/json',
+                "Authorization": Cookies.get('Authorization')
             },
-          }).then((response) => response.json());
-          console.log(cigar)
-          if (!Array.isArray(cigar)){
-            throw Error("No")
+        }).then((response) => response.json());
+        console.log(userdata)
+          if (!Cookies.get("Authorization")){
+            console.error("ERROR: ", error);
           }
-          return {User}
+          {User}
+        return userdata
     } catch (error) {
         return redirect('/login')
     }
 }
 export default function User (){
-
+    const user = useLoaderData();
+    function displayName(){
+        if (user.first_name){
+            return user.first_name
+        }
+        else {return user.username}
+    };
     return (
         <>
             <h1>My Profile</h1>
+            <p>Welcome {displayName()}!</p>
         </>
     )
 }
