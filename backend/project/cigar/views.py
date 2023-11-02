bab328c3763471bc9b381777e51cc546a25ee34c
Austin Berry <aberry52098@gmail.com>
2023-11-02 15:32:32 -0400

    add brand viewset

--- backend/project/cigar/views.py
+++ backend/project/cigar/views.py
@@ -1,9 +1,13 @@
 from rest_framework import viewsets
-from .serializers import CigarSerializer
+from .serializers import CigarSerializer, BrandSerializer
 from rest_framework.permissions import IsAuthenticated
-from .models import Cigar
+from .models import Cigar, Brand
 # Create your views here.
 class CigarViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated,]
     queryset = Cigar.objects.all().order_by('name')
     serializer_class = CigarSerializer
+
+class BrandViewset(viewsets.ModelViewSet):
+    queryset = Brand.objects.all()
+    serializer_class = BrandSerializer
