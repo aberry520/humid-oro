c40116063a2d282aafc32aa64bf231fbee1be773
Austin Berry <aberry52098@gmail.com>
2023-11-01 21:57:04 -0400

    router register cigar viewset

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from project.cigar import views
+# from django.conf import settings
+
+router = routers.DefaultRouter()
+router.register(r'cigar', views.CigarViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls'))
 ]
