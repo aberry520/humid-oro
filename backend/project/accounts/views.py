83d056865ce3ed22a4b19d5aa73281a2aa190af8
Austin Berry <aberry52098@gmail.com>
2023-11-02 15:38:56 -0400

    create profile and user viewset

--- backend/project/accounts/views.py
+++ backend/project/accounts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Profile
+from .serializers import ProfileSerializer, UserDetailsSerializer
+from django.contrib.auth.models import User
+from dj_rest_auth.views import UserDetailsView
 
 # Create your views here.
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all().order_by('id')
+    users = User.objects.all().select_related('user_id')
+    serializer_class = ProfileSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserDetailsSerializer
+    
