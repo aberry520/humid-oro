99d7f54fba0f71656a2a8a55d23d8ab22f553a95
Austin Berry <aberry52098@gmail.com>
2023-11-02 15:35:36 -0400

    create brand serializer

--- backend/project/cigar/serializers.py
+++ backend/project/cigar/serializers.py
@@ -1,6 +1,11 @@
-from .models import Cigar
+from .models import Cigar, Brand
 from rest_framework import serializers
 class CigarSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cigar
         fields = ['name', 'brand', 'rating_avg', 'time_avg', 'origin', 'wrapper', 'filler', 'binder','strength','wrapper_color','length','gauge','shape','description']
+
+class BrandSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Brand
+        fields = ['name']
