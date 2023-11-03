3030176fdc33ee46fcf524cd3f89012382084c22
Austin Berry <aberry52098@gmail.com>
2023-11-03 09:00:04 -0400

    update media settings

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 import environ
 env = environ.Env()
@@ -165,3 +165,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_AUTH = {
     'USER_DETAILS_SERIALIZER': 'project.accounts.serializers.UserDetailsSerializer',
 }
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "")
+MEDIA_URL = "/profile/"
