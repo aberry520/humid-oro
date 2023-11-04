982866942cce624fc6d883c136c4cfbcf3ec828f
Austin Berry <aberry52098@gmail.com>
2023-11-04 18:54:33 -0400

    add select and options for filters

--- frontend/humid-oro/src/components/Filters.jsx
+++ frontend/humid-oro/src/components/Filters.jsx
@@ -3,14 +3,17 @@ import styled from "styled-components";
 const Filter = styled.div`
     border: black solid;
     margin: 5px;
-    display: flex;
-    justify-content: center;
+    /* display: flex; */
+    /* justify-content: center; */
+    /* flex-direction: column; */
+    /* align-items: center; */
     max-width: 20vw;
     min-width: fit-content;
     min-height: 80vh;
     max-height: 85vh;
     h1{
         margin: 1rem;
+        text-align: center;
     }
 `
 export const Filters = () => {
@@ -19,6 +22,15 @@ export const Filters = () => {
         <>
         <Filter>
         <h1>Filters</h1>
+        <h3>Country of Orgin</h3>
+        <select>
+            <option>Country Of Orgin</option>
+        
+        </select>
+        <h3>Strength</h3>
+        <select><option>Strength</option></select>
+        <h3>Brand</h3>
+        <select><option>Brand</option></select>
         </Filter>
         </>
     )
