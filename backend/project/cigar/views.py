443a08ebab914e8c6e04c3b99b72d1cd58916783
Austin Berry <aberry52098@gmail.com>
2023-11-06 19:49:14 -0500

    added search and ordering to cigar view set

--- backend/project/cigar/views.py
+++ backend/project/cigar/views.py
@@ -1,13 +1,18 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics, mixins
+import django_filters.rest_framework
 from .serializers import CigarSerializer, BrandSerializer
 from rest_framework.permissions import IsAuthenticated
 from .models import Cigar, Brand
+from rest_framework.filters import SearchFilter, OrderingFilter
 # Create your views here.
 class CigarViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated,]
-    queryset = Cigar.objects.all().order_by('name')
+    queryset = Cigar.objects.all()
     serializer_class = CigarSerializer
-
+    filterset_fields = ['id','name','strength']
+    search_fields = ['name']
+    filter_backends = (SearchFilter, OrderingFilter)
+    
 class BrandViewset(viewsets.ModelViewSet):
     queryset = Brand.objects.all()
     serializer_class = BrandSerializer
