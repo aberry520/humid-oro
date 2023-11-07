bcc52ed889f186f478f9e2c1c835db15bdf3e928
Austin Berry <aberry52098@gmail.com>
2023-11-07 17:54:37 -0500

    allow any permission class

--- backend/project/cigar/views.py
+++ backend/project/cigar/views.py
@@ -1,12 +1,12 @@
 from rest_framework import viewsets, generics, mixins
 import django_filters.rest_framework
 from .serializers import CigarSerializer, BrandSerializer
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Cigar, Brand
 from rest_framework.filters import SearchFilter, OrderingFilter
 # Create your views here.
 class CigarViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticated,]
+    permission_classes = [AllowAny]
     queryset = Cigar.objects.all()
     serializer_class = CigarSerializer
     filterset_fields = ['id','name','strength']
@@ -14,5 +14,6 @@ class CigarViewSet(viewsets.ModelViewSet):
     filter_backends = (SearchFilter, OrderingFilter)
     
 class BrandViewset(viewsets.ModelViewSet):
+    permission_classes = [AllowAny]
     queryset = Brand.objects.all()
     serializer_class = BrandSerializer
