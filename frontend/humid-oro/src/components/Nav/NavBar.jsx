d02a238d07b67506395718f83ff28782a7ef8ae3
Austin Berry <aberry52098@gmail.com>
2023-11-07 17:58:18 -0500

    add search logic

--- frontend/humid-oro/src/components/Nav/NavBar.jsx
+++ frontend/humid-oro/src/components/Nav/NavBar.jsx
@@ -1,29 +1,59 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+// import { Search } from "react-router-dom";
 import styled from "styled-components";
 
 const Nav = styled.div`
-    display: flex;
-    gap: 10px;
-    max-width: 1000px;
-    justify-content: space-around;
-    margin: auto;
-`
+  display: flex;
+  gap: 10px;
+  max-width: 1000px;
+  justify-content: space-around;
+  margin: auto;
+`;
 const NavBorder = styled.div`
-    border: black solid;
-    margin: 0;
-`
+  border: black solid;
+  margin: 0;
+`;
 
 export const NavBar = () => {
+  const [search, setSearch] = useState("");
+  const [lastSearch, setLastSearch] = useState("");
+  const navigate = useNavigate();
 
-    return (
-        <>
-        <NavBorder>
-            <Nav>
-                <Link to={'/'}><p>Home</p></Link>
-                <input type="search" placeholder="Search"/>
-                <Link to={'user/'}><p>Profile</p></Link>
-            </Nav>
-        </NavBorder>    
-        </>
-    )
-}
+  const handleChangeSearch = (e) => {
+    setSearch(e.target.value);
+    setLastSearch(e.target.value)
+  };
+
+  const handleSearchClick = (e) => {
+    if (search === ""){
+        navigate(`/search/${lastSearch}`);
+    }else{
+        navigate(`/search/${search}`);
+        setSearch("")
+    }
+  };
+  return (
+    <>
+      <NavBorder>
+        <Nav>
+          <Link to={"/"}>
+            <p>Home</p>
+          </Link>
+          <form>
+            <input
+              type="search"
+              placeholder="Search"
+              onChange={handleChangeSearch}
+              value={search}
+            />
+            <button type="button" name="searchButton" onClick={(e)=>{handleSearchClick(e)}}>Search</button>
+          </form>
+          <Link to={"user/"}>
+            <p>Profile</p>
+          </Link>
+        </Nav>
+      </NavBorder>
+    </>
+  );
+};
