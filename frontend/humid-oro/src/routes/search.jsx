f8250818750615644024f90fb4aa61e954f74a82
Austin Berry <aberry52098@gmail.com>
2023-11-08 16:05:18 -0500

    clean up comments

--- frontend/humid-oro/src/routes/search.jsx
+++ frontend/humid-oro/src/routes/search.jsx
@@ -1,12 +1,9 @@
-import { useLoaderData, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { Filters } from "../components/Filters";
 import { Cigars } from "../components/Cigars";
 import styled from "styled-components";
-import Cookies from "js-cookie";
-import { NavBar } from "../components/Nav/NavBar";
 import { useEffect } from "react";
 import { useState } from "react";
-import { Loading } from "../components/Loading";
 
 const HomeDiv = styled.div`
   border: black solid;
@@ -15,27 +12,6 @@ const HomeDiv = styled.div`
   grid-template-columns: 1fr 3fr;
   max-width: 90vw;
 `;
-// export async function loader() {
-//     console.log("search")
-//     const [search, setSearch] = useState("");
-//     const params = useParams();
-//     console.log({params})
-//     const handleChangeSearch = (e) => {
-//       setSearch(e.target.value);
-//     };
-//     const handleSearchSubmit = async (e) => {
-//       e.preventDefault();
-//       const url = `http://127.0.0.1:8001/cigar/`;
-//       try {
-//         const response = await fetch(url);
-//         const result = await response.json();
-//         console.log(result)
-//         return result;
-//       } catch (error) {
-//         console.error(error);
-//       }
-//     };
-// }
 
 export default function Search({}) {
   const params = useParams();
@@ -45,7 +21,7 @@ export default function Search({}) {
     try {
       const response = await fetch(url);
       const result = await response.json();
-      setCigarsSearch(result)
+      setCigarsSearch(result);
       return result;
     } catch (error) {
       console.error(error);
@@ -55,11 +31,8 @@ export default function Search({}) {
     getSearch();
   }, [params.search]);
 
-  
-
   return (
     <>
-      {/* <NavBar/> */}
       <HomeDiv>
         <Filters />
         <Cigars cigarsSearch={cigarsSearch} />
