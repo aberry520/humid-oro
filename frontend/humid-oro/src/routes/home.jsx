2ee1ba4d68d8d64fbda81d60bce70de7616a76c6
Austin Berry <aberry52098@gmail.com>
2023-11-08 16:04:47 -0500

    remove comments

--- frontend/humid-oro/src/routes/home.jsx
+++ frontend/humid-oro/src/routes/home.jsx
@@ -2,12 +2,8 @@ import { useLoaderData } from "react-router-dom";
 import { Filters } from "../components/Filters";
 import { Cigars } from "../components/Cigars";
 import styled from "styled-components";
-import Cookies from "js-cookie";
-import { NavBar } from "../components/Nav/NavBar";
-
 
 const HomeDiv = styled.div`
-  border: black solid;
   margin: 10px auto;
   display: grid;
   grid-template-columns: 1fr 3fr;
@@ -15,26 +11,26 @@ const HomeDiv = styled.div`
 `;
 
 export async function loader() {
-  
-  const url = 'http://127.0.0.1:8001/cigar/?page=1';
+  const url = "http://127.0.0.1:8001/cigar/";
   try {
-      const response = await fetch(url);
-      const result = await response.json();
-      {Home}
-      return result
+    const response = await fetch(url);
+    const result = await response.json();
+    {
+      Home;
+    }
+    return result;
   } catch (error) {
-      console.error(error);
+    console.error(error);
   }
 }
 export default function Home() {
   const cigarsHome = useLoaderData();
-  // console.log(cigars)
+
   return (
     <>
-      {/* <NavBar/> */}
       <HomeDiv>
         <Filters />
-        <Cigars cigarsHome={cigarsHome}/>
+        <Cigars cigarsHome={cigarsHome} />
       </HomeDiv>
     </>
   );
