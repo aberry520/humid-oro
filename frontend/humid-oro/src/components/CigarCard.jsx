d906286cab394073249011d3e5934a690d4ee392
Austin Berry <aberry52098@gmail.com>
2023-11-08 14:27:38 -0500

    add some styling

--- frontend/humid-oro/src/components/CigarCard.jsx
+++ frontend/humid-oro/src/components/CigarCard.jsx
@@ -1,22 +1,28 @@
 import styled from "styled-components";
 
-const NavBorder = styled.div`
-  border: black solid;
+const Card = styled.div`
+  border: solid #bf976d;
   margin: 0;
   min-height: 300px;
   max-height: fit-content;
   max-width: 30ch;
   display: flex;
   flex-wrap: wrap;
+  justify-content: center;
   /* flex-direction: column; */
+  background-color: #364F59;
+  box-shadow: 1px 29px 81px 0px rgba(0,0,0,0.75);
+  img{
+    margin-top: 15px;
+  }
 `;
 
 export const CigarCard = ({cigars}) => {
 
   return (
     <>
-      <NavBorder>
-        <h1>Cigar Card</h1>
+      <Card>
+        {/* <h1>Cigar Card</h1> */}
         <img src={cigars.thumbnail} />
         <h2>{cigars.name}</h2>
         <h3>
@@ -26,9 +32,9 @@ export const CigarCard = ({cigars}) => {
             <br/>
             Length: {cigars.length}"
             <br/>
-            Ring Gauge: {cigars.ringGauge}
+            Ring Gauge: {cigars.gauge}
         </h3>
-      </NavBorder>
+      </Card>
     </>
   );
 };
