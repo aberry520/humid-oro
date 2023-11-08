c50d0f5c1a725f3290528fc393b3184070274f34
Austin Berry <aberry52098@gmail.com>
2023-11-08 14:29:36 -0500

    create search component

---
+++ frontend/humid-oro/src/components/Nav/Search.jsx
@@ -0,0 +1,57 @@
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+// import { Search } from "react-router-dom";
+import styled from "styled-components";
+
+const SearchDiv = styled.div`
+    display: flex;
+`;
+
+export const Search = () => {
+  const [search, setSearch] = useState("");
+  const [lastSearch, setLastSearch] = useState("");
+  const navigate = useNavigate();
+
+  const handleChangeSearch = (e) => {
+    setSearch(e.target.value);
+    setLastSearch(e.target.value);
+  };
+
+  const handleSearchClick = (e) => {
+    if (search === "") {
+      navigate(`/search/${lastSearch}`);
+    } else {
+      navigate(`/search/${search}`);
+      setSearch("");
+    }
+  };
+  const handleEnter = (e) => {
+    const code = e.keyCode || e.which;
+    if (code === 13) {
+      navigate(`/search/${search}`);
+      setSearch("");
+    }
+  };
+  return (
+    <>
+      <SearchDiv>
+        <input
+          type="search"
+          placeholder="Search"
+          onChange={handleChangeSearch}
+          onKeyDown={handleEnter}
+          value={search}
+        />
+        <button
+          type="button"
+          name="searchButton"
+          onClick={(e) => {
+            handleSearchClick(e);
+          }}
+        >
+          Search
+        </button>
+      </SearchDiv>
+    </>
+  );
+};
