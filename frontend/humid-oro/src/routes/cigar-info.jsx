9260731e698df164bd102c6b869be15073866657
Austin Berry <aberry52098@gmail.com>
2023-11-08 14:33:57 -0500

    create cigar info route

---
+++ frontend/humid-oro/src/routes/cigar-info.jsx
@@ -0,0 +1,50 @@
+import { useLoaderData, useParams } from "react-router-dom";
+import { Filters } from "../components/Filters";
+import { Cigars } from "../components/Cigars";
+import styled from "styled-components";
+import Cookies from "js-cookie";
+import { NavBar } from "../components/Nav/NavBar";
+import { Pagination } from "../components/Pagination";
+import { useEffect, useState } from "react";
+
+const HomeDiv = styled.div`
+  /* border: black solid; */
+  margin: 10px auto;
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  max-width: 90vw;
+`;
+function Cigar({ cigarInfo }) {
+  return (
+    <>
+      <HomeDiv>
+        <h1>{cigarInfo.name}</h1>
+        <img src={cigarInfo.image} />
+      </HomeDiv>
+    </>
+  );
+}
+export default function CigarInfo() {
+  const params = useParams();
+  const [cigarInfo, setCigarInfo] = useState();
+  async function getSearch() {
+    const url = `http://127.0.0.1:8001/cigar/${params.id}`;
+    try {
+      const response = await fetch(url);
+      const result = await response.json();
+      setCigarInfo(result);
+      console.log(result);
+      return result;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  useEffect(() => {
+    getSearch();
+  }, [params.id]);
+  return (
+    <>
+      <Cigar cigarInfo={cigarInfo} />
+    </>
+  );
+}
