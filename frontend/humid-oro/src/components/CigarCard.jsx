33f428ef1f88a467b57fa86a32d22de019bf57c2
Austin Berry <aberry52098@gmail.com>
2023-11-08 16:01:04 -0500

    update style

--- frontend/humid-oro/src/components/CigarCard.jsx
+++ frontend/humid-oro/src/components/CigarCard.jsx
@@ -5,34 +5,43 @@ const Card = styled.div`
   margin: 0;
   min-height: 300px;
   max-height: fit-content;
-  max-width: 30ch;
+  max-width: 25ch;
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
+  padding: 10px;
   /* flex-direction: column; */
-  background-color: #364F59;
-  box-shadow: 1px 29px 81px 0px rgba(0,0,0,0.75);
-  img{
+  background-color: #364f59;
+  box-shadow: 1px 29px 81px 0px rgba(0, 0, 0, 0.75);
+  img {
     margin-top: 15px;
+    width: 80%;
+    height: auto;
+  }
+  h2 {
+    font-size: medium;
+    margin: 0;
+  }
+  h3 {
+    font-size: small;
+    margin: 0;
   }
 `;
 
-export const CigarCard = ({cigars}) => {
-
+export const CigarCard = ({ cigars }) => {
   return (
     <>
       <Card>
-        {/* <h1>Cigar Card</h1> */}
         <img src={cigars.thumbnail} />
         <h2>{cigars.name}</h2>
         <h3>
-            Country: {cigars.country}
-            <br/>
-            Strength: {cigars.strength}
-            <br/>
-            Length: {cigars.length}"
-            <br/>
-            Ring Gauge: {cigars.gauge}
+          Country: {cigars.origin}
+          <br />
+          Strength: {cigars.strength}
+          <br />
+          {/* Length: {cigars.length}"
+          <br />
+          Ring Gauge: {cigars.gauge} */}
         </h3>
       </Card>
     </>
