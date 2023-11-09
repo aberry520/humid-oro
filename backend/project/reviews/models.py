13f0cbea1f55818b51459c5d42375c35c3df0d59
Austin Berry <aberry52098@gmail.com>
2023-11-09 13:34:46 -0500

    fix time in review model

--- backend/project/reviews/models.py
+++ backend/project/reviews/models.py
@@ -7,7 +7,7 @@ class Review(models.Model):
     cigar = models.ForeignKey(Cigar, on_delete=models.CASCADE, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     review = models.CharField(max_length=300)
-    time = models.IntegerField()
-    rating = models.IntegerField()
+    time = models.IntegerField(null=True)
+    rating = models.FloatField()
     def __str__(self):
         return self.user.username
