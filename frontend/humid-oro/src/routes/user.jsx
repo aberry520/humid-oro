b0bcc37d41f3e9c7c7774185d27f0457a250d6c4
Austin Berry <aberry52098@gmail.com>
2023-11-09 13:41:05 -0500

    reformat

--- frontend/humid-oro/src/routes/user.jsx
+++ frontend/humid-oro/src/routes/user.jsx
@@ -1,53 +1,57 @@
-import { redirect, useLoaderData } from "react-router-dom"
-import Cookies from 'js-cookie'
+import { redirect, useLoaderData } from "react-router-dom";
+import Cookies from "js-cookie";
 import { useAuth } from "../AuthContext";
 import { LogOut } from "../components/LogOut";
 import styled from "styled-components";
 
 const Profile = styled.div`
-    /* border: black solid; */
-    margin: auto;
-    width: fit-content;
+  border: black solid;
+  background-color: #364f59;
+
+  margin: auto;
+  width: fit-content;
 `;
 
 export async function loader() {
-    // const {isAuth} = useAuth();
-    // const token = localStorage.getItem("access_token");
-    console.log(Cookies.get())
-    try{
-        const url = `http://127.0.0.1:8001/dj-rest-auth/user/`
-        const userdata = await fetch(url, {
-            headers: {
-                'Content-Type': 'application/json',
-                "Authorization": Cookies.get('Authorization')
-            },
-        }).then((response) => response.json());
-        console.log(userdata)
-          if (!Cookies.get("Authorization")){
-            console.error("ERROR: ", error);
-          }
-          {User}
-        return userdata
-    } catch (error) {
-        return redirect('/login')
+  console.log(Cookies.get());
+  try {
+    const url = `http://127.0.0.1:8001/dj-rest-auth/user/`;
+    const userdata = await fetch(url, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: Cookies.get("Authorization"),
+      },
+    }).then((response) => response.json());
+    console.log(userdata);
+    Cookies.set("UserID", `${userdata.id}`);
+    if (!Cookies.get("Authorization")) {
+      console.error("ERROR: ", error);
     }
+    {
+      User;
+    }
+    return userdata;
+  } catch (error) {
+    return redirect("/login");
+  }
 }
-export default function User (){
-    const user = useLoaderData();
-    function displayName(){
-        if (user.first_name){
-            return user.first_name
-        }
-        else {return user.username}
-    };
-    return (
-        <>
-        <Profile>
-            <h1>My Profile</h1>
-            <LogOut/>
-            <p>Welcome {displayName()}!</p>
-            <img src={user.profile.avatar}/>
-        </Profile>
-        </>
-    )
+export default function User() {
+  const user = useLoaderData();
+  function displayName() {
+    if (user.first_name) {
+      return user.first_name;
+    } else {
+      return user.username;
+    }
+  }
+  return (
+    <>
+      <Profile>
+        <h1>My Profile</h1>
+        <LogOut />
+        <p>Welcome {displayName()}!</p>
+        <img src={user.profile.avatar} />
+      </Profile>
+    </>
+  );
 }
