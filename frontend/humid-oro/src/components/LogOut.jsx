8112e33f9ba4336d2dbbd9d231190fc64a316dfa
Austin Berry <aberry52098@gmail.com>
2023-11-09 13:36:52 -0500

    remove userid cookie

--- frontend/humid-oro/src/components/LogOut.jsx
+++ frontend/humid-oro/src/components/LogOut.jsx
@@ -2,28 +2,29 @@ import { useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
 import { useAuth } from "../AuthContext";
 export const LogOut = () => {
-    const {setIsAuth} = useAuth();
-    const navigate = useNavigate();
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const url = 'http://127.0.0.1:8001/dj-rest-auth/logout/';
-        const data = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                "Authorization": `${Cookies.get('Authorization')}`
-            },
-        }).then((response) => response.json());
-        setIsAuth(false);
-        Cookies.remove("Authorization");
-        console.log(data);
-        navigate('/login');
-    }
-    return (
-        <>
-            <form onSubmit={handleSubmit}>
-                <button type="submit">Log Out</button>
-            </form>
-        </>
-    )
-}
+  const { setIsAuth } = useAuth();
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const url = "http://127.0.0.1:8001/dj-rest-auth/logout/";
+    const data = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `${Cookies.get("Authorization")}`,
+      },
+    }).then((response) => response.json());
+    setIsAuth(false);
+    Cookies.remove("Authorization");
+    Cookies.remove("UserID");
+    console.log(data);
+    navigate("/login");
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <button type="submit">Log Out</button>
+      </form>
+    </>
+  );
+};
