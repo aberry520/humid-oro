5f5065777bce874780467a8b5e330267b3fc4d2d
Austin Berry <aberry52098@gmail.com>
2023-11-10 10:22:57 -0500

    creat user review item

---
+++ frontend/humid-oro/src/components/UserReviewItem.jsx
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+const Cigar = styled.div`
+  border: solid black;
+  display: grid;
+  grid-template-columns: 1fr, 1fr, 1fr;
+`;
+export const UserReviewItem = ({ review }) => {
+  return (
+    <>
+      <Cigar>
+        <h2>{review.cigar.name}</h2>
+        <h3>
+          Brand: {review.cigar.brand.name}
+          <br />
+          Country: {review.cigar.origin}
+          <br />
+          Strength: {review.cigar.strength}
+          <br />
+          Length: {review.cigar.length}"
+          <br />
+          Ring Gauge: {review.cigar.gauge}
+          <br />
+          Rating: {review.rating}
+          <br />
+          Smoke Time: {review.time} minutes
+        </h3>
+        <h3>Review:</h3>
+        <p>{review.review}</p>
+      </Cigar>
+    </>
+  );
+};
