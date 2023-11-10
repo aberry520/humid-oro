fc967f48165eb2237169ead6ddda8eb68fc4d907
Austin Berry <aberry52098@gmail.com>
2023-11-10 10:18:10 -0500

    nest cigar details in review serializer

--- backend/project/reviews/serializers.py
+++ backend/project/reviews/serializers.py
@@ -1,6 +1,10 @@
 from .models import Review
 from rest_framework import serializers
+from project.cigar.models import Cigar
+from project.cigar.serializers import CigarSerializer
 class ReviewSerializer(serializers.ModelSerializer):
+    cigar = CigarSerializer()
     class Meta:
         model = Review
-        fields = "__all__"
+        fields = '__all__'
+
