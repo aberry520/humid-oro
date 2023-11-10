36cd05bee9882fcbe056892456a11a87ec73e87e
Austin Berry <aberry52098@gmail.com>
2023-11-10 10:14:30 -0500

    add brand details to cigar serializer

--- backend/project/cigar/serializers.py
+++ backend/project/cigar/serializers.py
@@ -1,11 +1,13 @@
 from .models import Cigar, Brand
 from rest_framework import serializers
-class CigarSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Cigar
-        fields = "__all__"
 
 class BrandSerializer(serializers.ModelSerializer):
     class Meta:
         model = Brand
+        fields = "__all__"
+
+class CigarSerializer(serializers.ModelSerializer):
+    brand = BrandSerializer()
+    class Meta:
+        model = Cigar
         fields = "__all__"
