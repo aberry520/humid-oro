f726f0110a2a3d2e38b05c6fa93d13d45a93480c
Austin Berry <aberry52098@gmail.com>
2023-11-11 08:34:47 -0500

    adding filter logic

--- frontend/humid-oro/src/routes/home.jsx
+++ frontend/humid-oro/src/routes/home.jsx
@@ -2,16 +2,17 @@ import { useLoaderData } from "react-router-dom";
 import { Filters } from "../components/Filters";
 import { Cigars } from "../components/Cigars";
 import styled from "styled-components";
+import { useEffect, useState } from "react";
 
 const HomeDiv = styled.div`
   margin: 10px auto;
   display: grid;
-  grid-template-columns: 1fr 3fr;
+  grid-template-columns: 1fr 4fr;
   max-width: 90vw;
 `;
 
 export async function loader() {
-  const url = "http://127.0.0.1:8001/cigar/";
+  const url = `http://127.0.0.1:8001/cigar/`;
   try {
     const response = await fetch(url);
     const result = await response.json();
@@ -24,13 +25,61 @@ export async function loader() {
   }
 }
 export default function Home() {
-  const cigarsHome = useLoaderData();
+  const cigarsHomeInit = useLoaderData();
+  const [cigarsHome, setCigarsHome] = useState(cigarsHomeInit);
+  const [cigarFilter, setCigarFilter] = useState();
+  const [origin, setOrigin] = useState();
+  const [strength, setStrength] = useState();
+
+  function strengthChange(e) {
+    const type = e.target.value;
+    if (e.target.value === type) {
+      console.log(e.target.value);
+      if (type != "Strength") {
+        const strength = cigarsHome.results.filter(
+          (cigar) => cigar.strength === type
+        );
+        setCigarFilter(strength);
+      } else {
+        setCigarFilter();
+      }
+    }
+  }
+  function originChange(e) {
+    const type = e.target.value;
+    setOrigin;
+    if (cigarFilter !== undefined) {
+      const multiFilter = cigarFilter.filter(
+        (cigar) => cigar.strength === type
+      );
+      setCigarFilter(multiFilter);
+      console.log(multiFilter);
+    } else {
+      if (e.target.value === type) {
+        console.log(e.target.value);
+
+        if (type != "Origin") {
+          const origin = cigarsHome.results.filter(
+            (cigar) => cigar.origin === type
+          );
+          setCigarFilter(origin);
+        } else {
+          setCigarFilter();
+        }
+      }
+    }
+  }
+  console.log(cigarFilter);
 
   return (
     <>
       <HomeDiv>
-        <Filters />
-        <Cigars cigarsHome={cigarsHome} />
+        <Filters
+          strengthChange={strengthChange}
+          originChange={originChange}
+          cigarsHome={cigarsHome}
+        />
+        <Cigars cigarsHome={cigarsHome} cigarFilter={cigarFilter} />
       </HomeDiv>
     </>
   );
