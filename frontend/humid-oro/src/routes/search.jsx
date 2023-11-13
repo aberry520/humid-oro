fb0b4c73ff076abd912751147a1ddefbfb1a37fa
Austin Berry <aberry52098@gmail.com>
2023-11-12 21:30:31 -0500

    added filter logic to search page as well

--- frontend/humid-oro/src/routes/search.jsx
+++ frontend/humid-oro/src/routes/search.jsx
@@ -6,16 +6,19 @@ import { useEffect } from "react";
 import { useState } from "react";
 
 const HomeDiv = styled.div`
-  border: black solid;
   margin: 10px auto;
   display: grid;
-  grid-template-columns: 1fr 3fr;
+  grid-template-columns: 1fr 4fr;
   max-width: 90vw;
 `;
 
 export default function Search({}) {
   const params = useParams();
   const [cigarsSearch, setCigarsSearch] = useState();
+  const [cigarFilter, setCigarFilter] = useState();
+  const [origin, setOrigin] = useState();
+  const [strength, setStrength] = useState();
+  const [brand, setBrand] = useState();
   async function getSearch() {
     const url = `http://127.0.0.1:8001/cigar/?search=${params.search}`;
     try {
@@ -31,11 +34,147 @@ export default function Search({}) {
     getSearch();
   }, [params.search]);
 
+  useEffect(() => {
+    if (origin !== undefined && origin !== "Origin") {
+      const originFilter = cigarsSearch.results.filter(
+        (cigar) => cigar.origin === origin
+      );
+      setCigarFilter(originFilter);
+    } else {
+      setCigarFilter();
+    }
+    if (strength !== undefined && strength !== "Strength") {
+      const multiFilter = cigarsSearch.results.filter(
+        (cigar) => cigar.strength === strength && cigar.origin === origin
+      );
+      setCigarFilter(multiFilter);
+    }
+    if (brand !== undefined && brand !== "Brand") {
+      const multiFilter = cigarsSearch.results.filter(
+        (cigar) => cigar.brand === brand && cigar.origin === origin
+      );
+      setCigarFilter(multiFilter);
+    }
+    if (
+      brand !== undefined &&
+      brand !== "Brand" &&
+      origin !== undefined &&
+      origin !== "Origin" &&
+      strength !== undefined &&
+      strength !== "Strength"
+    ) {
+      const multiFilter = cigarsSearch.results.filter(
+        (cigar) =>
+          cigar.brand.name === brand &&
+          cigar.strength === strength &&
+          cigar.origin === origin
+      );
+      setCigarFilter(multiFilter);
+    }
+  }, [origin]);
+
+  useEffect(() => {
+    if (strength !== undefined && strength !== "Strength") {
+      const strengthFilter = cigarsSearch.results.filter(
+        (cigar) => cigar.strength === strength
+      );
+      setCigarFilter(strengthFilter);
+    } else {
+      setCigarFilter();
+    }
+    if (origin !== undefined && origin !== "Origin") {
+      const multiFilter = cigarsSearch.results.filter(
+        (cigar) => cigar.origin === origin && cigar.strength === strength
+      );
+      setCigarFilter(multiFilter);
+    }
+    if (brand !== undefined && brand !== "Brand") {
+      const multiFilter = cigarsSearch.results.filter(
+        (cigar) => cigar.brand.name === brand && cigar.strength === strength
+      );
+      setCigarFilter(multiFilter);
+    }
+    if (
+      brand !== undefined &&
+      brand !== "Brand" &&
+      origin !== undefined &&
+      origin !== "Origin" &&
+      strength !== undefined &&
+      strength !== "Strength"
+    ) {
+      const multiFilter = cigarsSearch.results.filter(
+        (cigar) =>
+          cigar.brand.name === brand &&
+          cigar.strength === strength &&
+          cigar.origin === origin
+      );
+      setCigarFilter(multiFilter);
+    }
+  }, [strength]);
+
+  useEffect(() => {
+    if (brand !== undefined && brand !== "Brand") {
+      console.log("hi ", brand);
+      const brandFilter = cigarsSearch.results.filter(
+        (cigar) => cigar.brand.name === brand
+      );
+      setCigarFilter(brandFilter);
+    } else {
+      setCigarFilter();
+    }
+    if (origin !== undefined && origin !== "Origin") {
+      const multiFilter = cigarsSearch.results.filter(
+        (cigar) => cigar.origin === origin && cigar.origin === origin
+      );
+      setCigarFilter(multiFilter);
+    }
+    if (strength !== undefined && strength !== "Strength") {
+      const multiFilter = cigarsSearch.results.filter(
+        (cigar) => cigar.brand.name === brand && cigar.strength === strength
+      );
+      setCigarFilter(multiFilter);
+    }
+
+    if (
+      brand !== undefined &&
+      brand !== "Brand" &&
+      origin !== undefined &&
+      origin !== "Origin" &&
+      strength !== undefined &&
+      strength !== "Strength"
+    ) {
+      const multiFilter = cigarsSearch.results.filter(
+        (cigar) =>
+          cigar.brand.name === brand &&
+          cigar.strength === strength &&
+          cigar.origin === origin
+      );
+      setCigarFilter(multiFilter);
+    }
+  }, [brand]);
+
+  function strengthChange(e) {
+    const type = e.target.value;
+    setStrength(type);
+  }
+  function originChange(e) {
+    const type = e.target.value;
+    setOrigin(type);
+  }
+  function brandChange(e) {
+    const type = e.target.value;
+    setBrand(type);
+  }
   return (
     <>
       <HomeDiv>
-        <Filters />
-        <Cigars cigarsSearch={cigarsSearch} />
+        <Filters
+          strengthChange={strengthChange}
+          originChange={originChange}
+          brandChange={brandChange}
+          cigarsSearch={cigarsSearch}
+        />
+        <Cigars cigarsSearch={cigarsSearch} cigarFilter={cigarFilter} />
       </HomeDiv>
     </>
   );
