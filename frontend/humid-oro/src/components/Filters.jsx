4c4237380c0a82009abbe12e04baf0d4106a3ed2
Austin Berry <aberry52098@gmail.com>
2023-11-12 21:26:42 -0500

    add logic to handle origins and brands and added options to strength

--- frontend/humid-oro/src/components/Filters.jsx
+++ frontend/humid-oro/src/components/Filters.jsx
@@ -25,22 +25,61 @@ const Filter = styled.div`
     text-align: center;
   }
 `;
-export const Filters = () => {
+export const Filters = ({
+  strengthChange,
+  originChange,
+  brandChange,
+  cigarsHome,
+  cigarsSearch,
+}) => {
+  // console.log(cigarsHome);
+  const allOrigins = [];
+  cigarsHome?.results.map((cigar) => {
+    allOrigins.push(cigar.origin);
+  });
+  const uniqueOrigins = [...new Set(allOrigins)];
+
+  const allBrands = [];
+  cigarsHome?.results.map((cigar) => {
+    allBrands.push(cigar.brand.name);
+  });
+  const uniqueBrands = [...new Set(allBrands)];
+  // console.log("New: ", allOrigins);
+  // console.log(uniqueOrigins);
   return (
     <>
       <Filter>
         <h1>Filters</h1>
-        <h3>Country of Orgin</h3>
-        <select>
-          <option>Country Of Orgin</option>
+        <h3>Country of Origin</h3>
+        <select onChange={originChange}>
+          <option value={"Origin"}>Country Of Origin</option>
+          {uniqueOrigins?.map((origin) => {
+            return (
+              <option key={origin} value={origin}>
+                {origin}
+              </option>
+            );
+          })}
         </select>
         <h3>Strength</h3>
-        <select>
-          <option>Strength</option>
+        <select onChange={strengthChange}>
+          <option value={"Strength"}>Strength</option>
+          <option value={"Mild"}>Mild</option>
+          <option value={"Mild-Medium"}>Mild-Medium</option>
+          <option value={"Medium"}>Medium</option>
+          <option value={"Medium-Full"}>Medium-Full</option>
+          <option value={"Full"}>Full</option>
         </select>
         <h3>Brand</h3>
-        <select>
-          <option>Brand</option>
+        <select onChange={brandChange}>
+          <option value={"Brand"}>Brand</option>
+          {uniqueBrands?.map((brand) => {
+            return (
+              <option key={brand} value={brand}>
+                {brand}
+              </option>
+            );
+          })}
         </select>
       </Filter>
     </>
