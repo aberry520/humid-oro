5ff41e81f00ea334f17341ea71db08d1c3c44257
Austin Berry <aberry52098@gmail.com>
2023-11-14 14:44:53 -0500

    add error handling and navigation after submitting review

--- frontend/humid-oro/src/components/ReviewForm.jsx
+++ frontend/humid-oro/src/components/ReviewForm.jsx
@@ -1,9 +1,19 @@
 import Cookies from "js-cookie";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+const Review = styled.div`
+  /* display: flex;
+  flex-direction: column; */
+`;
+
 export const ReviewForm = ({ cigarInfo }) => {
   const [review, setReview] = useState();
   const [rating, setRating] = useState();
   const [time, setTime] = useState();
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
   const handleChangeReview = (e) => {
     setReview(e.target.value);
   };
@@ -23,50 +33,62 @@ export const ReviewForm = ({ cigarInfo }) => {
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const url = "http://127.0.0.1:8001/reviewsadd/";
-    const data = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `${Cookies.get("Authorization")}`,
-      },
-      body: JSON.stringify(reviewInput),
-    }).then((response) => response.json());
-    console.log(data);
+    try {
+      const url = "http://127.0.0.1:8001/reviewsadd/";
+      const data = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `${Cookies.get("Authorization")}`,
+        },
+        body: JSON.stringify(reviewInput),
+      }).then((response) => response.json());
+      console.log(data);
+      if (data.id) {
+        navigate(`/user/`);
+      }
+    } catch (err) {
+      setError(
+        "There was an issue adding this review. Please check your entry and try again."
+      );
+    }
   };
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <textarea
-          name="review"
-          placeholder="Description"
-          rows="10"
-          cols="50"
-          required
-          onChange={handleChangeReview}
-        />
-        <input
-          name="time"
-          placeholder="Minutes Smoked"
-          type="number"
-          required
-          onChange={handleChangeTime}
-        />
-        <label htmlFor="Rating">
-          Rating: 0
-          <input
-            type="decimal"
-            name="rating"
-            min={0}
-            max={10}
-            step={0.5}
+      <Review>
+        {error && <h3>{error}</h3>}
+        <form onSubmit={handleSubmit}>
+          <textarea
+            name="review"
+            placeholder="Description"
+            rows="10"
+            cols="50"
             required
-            onChange={handleChangeRating}
+            onChange={handleChangeReview}
           />
-          10
-        </label>
-        <button type="submit">Review</button>
-      </form>
+          <input
+            name="time"
+            placeholder="Minutes Smoked"
+            type="number"
+            required
+            onChange={handleChangeTime}
+          />
+          <label htmlFor="Rating">
+            Rating: 0
+            <input
+              type="decimal"
+              name="rating"
+              min={0}
+              max={10}
+              step={0.5}
+              required
+              onChange={handleChangeRating}
+            />
+            10
+          </label>
+          <button type="submit">Review</button>
+        </form>
+      </Review>
     </>
   );
 };
