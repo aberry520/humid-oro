41ae913b47b5b030434e68bf3bf265c58ee26baf
Austin Berry <aberry52098@gmail.com>
2023-11-14 09:56:57 -0500

    remove max length and add default images

--- backend/project/cigar/models.py
+++ backend/project/cigar/models.py
@@ -7,21 +7,21 @@ class Brand(models.Model):
         return self.name
 
 class Cigar(models.Model):
-    name = models.CharField(max_length=50)
-    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, null=True)
+    name = models.CharField()
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
     rating_avg = models.FloatField(null=True)
     time_avg = models.IntegerField(null=True)
-    origin = models.CharField(max_length=50,null=True)
-    wrapper = models.CharField(max_length=50,null=True)
-    filler = models.CharField(max_length=50,null=True)
-    binder = models.CharField(max_length=50,null=True)
-    strength = models.CharField(max_length=50,null=True)
-    color = models.CharField(max_length=50,null=True)
+    origin = models.CharField(null=True)
+    wrapper = models.CharField(null=True)
+    filler = models.CharField(null=True)
+    binder = models.CharField(null=True)
+    strength = models.CharField(null=True)
+    color = models.CharField(null=True)
     length = models.FloatField(null=True)
     gauge = models.IntegerField(null=True)
-    shape = models.CharField(max_length=50, null=True)
+    shape = models.CharField(null=True)
     description = models.CharField(max_length=300, null=True)
-    image = models.URLField(null=True)
-    thumbnail = models.URLField(null=True)
+    image = models.URLField(null=True, default="https://media.istockphoto.com/id/173545983/photo/cigar-with-blank-label.jpg?s=170667a&w=0&k=20&c=ELrSKomP9GR5EsRaDAKgw-j4bwZ1edtz1if1YG5q-JM=")
+    thumbnail = models.URLField(null=True, default="https://media.istockphoto.com/id/173545983/photo/cigar-with-blank-label.jpg?s=170667a&w=0&k=20&c=ELrSKomP9GR5EsRaDAKgw-j4bwZ1edtz1if1YG5q-JM=")
     def __str__(self):
         return self.name
