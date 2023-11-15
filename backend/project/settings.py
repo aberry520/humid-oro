a77d6b8560da79591d2e309fd3c6bb2513695db2
Austin Berry <aberry52098@gmail.com>
2023-11-15 12:03:41 -0500

    change backend pagination size

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -25,7 +25,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 50,
+    'PAGE_SIZE': 30,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
 EMAIL_BACKEND ='django.core.mail.backends.console.EmailBackend'
