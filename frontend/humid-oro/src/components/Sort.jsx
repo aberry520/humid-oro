ecf18d2d653ea364a9ac386c53fcd4449e07da53
Austin Berry <aberry52098@gmail.com>
2023-11-16 09:42:26 -0500

    add responsiveness

--- frontend/humid-oro/src/components/Sort.jsx
+++ frontend/humid-oro/src/components/Sort.jsx
@@ -3,13 +3,13 @@ import styled from "styled-components";
 const Filter = styled.div`
   /* border: black solid; */
   /* background-color: #151414; */
-
   /* margin-left: 20px; */
   margin: 5px;
   /* display: flex; */
   /* justify-content: center; */
   /* flex-direction: column; */
   /* align-items: center; */
+  text-align: center;
   max-width: 20vw;
   min-width: fit-content;
   min-height: 80vh;
@@ -24,6 +24,34 @@ const Filter = styled.div`
     margin: 1rem;
     text-align: center;
   }
+  @media screen and (max-width: 1000px) {
+    min-height: fit-content;
+    display: flex;
+    flex: 0 1 auto;
+    gap: 10px;
+    margin: auto;
+    h1,
+    h3 {
+      font-size: 0.8em;
+      text-align: center;
+    }
+    button {
+      font-size: 0.5em;
+    }
+  }
+`;
+const Categories = styled.div`
+  /* display: flex; */
+  text-align: center;
+  .sort {
+    font-size: 1.25em;
+  }
+`;
+const Category = styled.div`
+  /* display: flex; */
+`;
+const Buttons = styled.div`
+  /* display: flex; */
 `;
 export const Sort = ({
   ascendingOrigin,
@@ -35,27 +63,35 @@ export const Sort = ({
 }) => {
   return (
     <>
-      <Filter>
-        <h1>Sort By</h1>
-        <h3>Country of Origin</h3>
-        <button onClick={ascendingOrigin} value={"origin"}>
-          Ascending
-        </button>
-        <button onClick={descendingOrigin} value={"-origin"}>
-          Descending
-        </button>
-        {/* <select onChange={originChange}>
+      <Categories>
+        <h1 className="sort">Sort By</h1>
+        <Filter>
+          <Category>
+            <h3>Country of Origin</h3>
+            <Buttons>
+              <button onClick={ascendingOrigin} value={"origin"}>
+                Ascending
+              </button>
+              <button onClick={descendingOrigin} value={"-origin"}>
+                Descending
+              </button>
+            </Buttons>
+          </Category>
+          {/* <select onChange={originChange}>
           <option value={"Origin"}>Country Of Origin</option>
           <option value={"Origin"}>Country Of Origin</option>
         </select> */}
-        <h3>Strength</h3>
-        <button onClick={ascendingStrength} value={"strength"}>
-          Ascending
-        </button>
-        <button onClick={descendingStrength} value={"-strength"}>
-          Descending
-        </button>
-        {/* <select onChange={strengthChange}>
+          <Category>
+            <h3>Strength</h3>
+            <Buttons>
+              <button onClick={ascendingStrength} value={"strength"}>
+                Ascending
+              </button>
+              <button onClick={descendingStrength} value={"-strength"}>
+                Descending
+              </button>
+            </Buttons>
+            {/* <select onChange={strengthChange}>
           <option value={"Strength"}>Strength</option>
           <option value={"Mild"}>Mild</option>
           <option value={"Mild-Medium"}>Mild-Medium</option>
@@ -63,17 +99,23 @@ export const Sort = ({
           <option value={"Medium-Full"}>Medium-Full</option>
           <option value={"Full"}>Full</option>
         </select> */}
-        <h3>Brand</h3>
-        <button onClick={ascendingBrand} value={"brand"}>
-          Ascending
-        </button>
-        <button onClick={descendingBrand} value={"-brand"}>
-          Descending
-        </button>
-        {/* <select onChange={brandChange}>
+          </Category>
+          <Category>
+            <h3>Brand</h3>
+            <Buttons>
+              <button onClick={ascendingBrand} value={"brand"}>
+                Ascending
+              </button>
+              <button onClick={descendingBrand} value={"-brand"}>
+                Descending
+              </button>
+            </Buttons>
+            {/* <select onChange={brandChange}>
           <option value={"Brand"}>Brand</option>
         </select> */}
-      </Filter>
+          </Category>
+        </Filter>
+      </Categories>
     </>
   );
 };
