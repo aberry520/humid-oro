5fb17ce791696e9f215c3a3b3cd16ad020d245cf
Austin Berry <aberry52098@gmail.com>
2023-11-16 09:40:09 -0500

    create and style footer

---
+++ frontend/humid-oro/src/components/Footer.jsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+const Nav = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  margin: auto;
+  min-height: 15vh;
+  .footer {
+    opacity: 40%;
+    position: absolute;
+    z-index: -1;
+  }
+  .logo {
+    margin-top: 50px;
+  }
+`;
+const NavBorder = styled.div`
+  margin: 0;
+  display: flex;
+`;
+
+export const Footer = () => {
+  return (
+    <>
+      <NavBorder>
+        <Nav>
+          <img className="logo" src="../public/humidoro.svg" />
+          <img className="footer" src="../public/footer.svg" />
+        </Nav>
+      </NavBorder>
+    </>
+  );
+};
