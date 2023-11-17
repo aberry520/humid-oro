a579661fe1d91dba8e8c64dfdb0e042a6fd142ad
Austin Berry <aberry52098@gmail.com>
2023-11-17 11:17:08 -0500

    style and add error image

--- frontend/humid-oro/src/routes/error-page.jsx
+++ frontend/humid-oro/src/routes/error-page.jsx
@@ -1,25 +1,31 @@
 import { NavBar } from "../components/Nav/NavBar";
-
+import styled from "styled-components";
+const Error = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  img {
+    max-width: 500px;
+  }
+`;
 export default function ErrorPage() {
   return (
     <>
       <NavBar />
-      <h1>Oh no! An error occured!</h1>
-      <h2>
-        Please tell us what went wrong so that we can make your humid-oro
-        experience better!
-      </h2>
-      <form>
-        <textarea
-          name="error"
-          placeholder="Report Issue Here"
-          rows="10"
-          cols="50"
-          required
-        />
-        <br />
-        <button type="button">Submit</button>
-      </form>
+      <Error>
+        <img src="../../public/error.svg" />
+        <form>
+          <textarea
+            name="error"
+            placeholder="Report Issue Here"
+            rows="10"
+            cols="50"
+            required
+          />
+          <br />
+          <button type="button">Submit</button>
+        </form>
+      </Error>
     </>
   );
 }
