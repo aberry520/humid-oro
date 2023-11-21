1d4a90c153b30954df8a27caac8242350aa5512e
Austin Berry <aberry52098@gmail.com>
2023-11-21 18:52:53 -0500

    allowed host

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -40,9 +40,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1']
+ALLOWED_HOSTS = ['https://www.humid-oro.com/']
 
 
 # Application definition
